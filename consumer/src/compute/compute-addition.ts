03da5694f5373489308b0058b1b9bb5d0f4aa336
wenderpmachado <wenderpmachado@gmail.com>
2021-09-16 21:18:56 -0300

    feat(consumer): added core functions + basic unit tests

---
+++ consumer/src/compute/compute-addition.ts
@@ -0,0 +1,21 @@
+interface SplitedExpression {
+    operations: string[];
+    numbers: number[];
+}
+
+/**
+ * Split the expression into arrays divided into operations and numbers
+ * @param expression The arithmetic expression
+ * @returns SplitedExpression
+ */
+function splitExpression(expression: string): SplitedExpression {
+    const operations = expression.split(/\d/).slice(1);
+    const numbers = expression.split(/\D/).slice(0, -1).map(Number);
+
+    return {
+        operations,
+        numbers
+    };
+}
+
+export default splitExpression;
